24382de8bcb1bbbaf2179be7cadd24e809108085
Murilo Messias de Camargo <murilo.messias@gmail.com>
2024-02-12 12:07:09 -0300

    format example

---
+++ src/ui/button.tsx
@@ -0,0 +1,11 @@
+export const Button = () => {
+	/* Expected 
+		 <button className='bg-blue-50 text-black'>Button</button> 
+	*/
+
+	// Output
+	return <button
+		className="text-black bg-blue-50">
+
+		Button</button>
+}
